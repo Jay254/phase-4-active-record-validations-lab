fbedefdca7e0dd67531ceb24a268cdf28687e303
Yegon Japheth Kiptoo <jyegon504@gmail.com>
2023-06-24 18:06:53 +0300

    Done basic and customs validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :title_is_clickbait
+  
+    def title_is_clickbait
+      clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+      if title.present? && !clickbait_phrases.any? { |phrase| title.include?(phrase) }
+        errors.add(:title, "should be clickbait-y")
+      end
+    end
+  end
